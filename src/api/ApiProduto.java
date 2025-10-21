e959ed9b6aa4b0a60e328aae63ea18739e021483
rafaelOlegarioBisoffi <rafaelolegariobisoffi@gmail.com>
2025-10-20 22:39:50 -0300

    api

--- src/api/ApiProduto.java
+++ src/api/ApiProduto.java
@@ -5,6 +5,7 @@ import static spark.Spark.*;
 import com.google.gson.Gson;
 
 import dao.ProdutoDAO;
+import model.Produto;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -39,6 +40,85 @@ public class ApiProduto {
             }
         });
 
+         get("/produto/:id", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Long id = Long.parseLong(request.params(":id"));
+                Produto p = dao.buscarPorId(id);
+                if(p != null) {
+                    return gson.toJson(p);
+                } else {
+                    response.status(404);
+                    return "{\"mensagem\":\"Produto não encontrado\"}";
+                }
+            }
+        });
+
+        post("/produto", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                
+                Produto body = gson.fromJson(request.body(), Produto.class);
+
+                if (body == null) {
+                    response.status(400);
+                    return "{\"mensagem\":\"Requisição inválida\"}";
+                }
+
+                dao.inserir(body);
+                response.status(201);
+                return gson.toJson(body);
+            }
+        });
+
+        
+        put("/produto/:id", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Long id = Long.parseLong(request.params(":id"));
+
+                Produto produtoExistente = dao.buscarPorId(id);
+                if (produtoExistente == null) {
+                    response.status(404);
+                    return "{\"mensagem\":\"Produto não encontrado\"}";
+                }
+                Produto body = gson.fromJson(request.body(), Produto.class);
+                  if (body == null) {
+                    response.status(400);
+                    return "{\"mensagem\":\"Requisição inválida\"}";
+                }
+                produtoExistente.setEstoque(body.getEstoque());
+                produtoExistente.setNome(body.getNome());
+                produtoExistente.setPreco(body.getPreco());
+
+              
+
+                dao.atualizar(produtoExistente);
+                response.status(200);
+                return gson.toJson(body);
+            }
+        });
+
+          delete("/produto/:id", new Route() {
+            @Override
+            public Object handle(Request request, Response response) {
+                Long id = Long.parseLong(request.params(":id"));
+
+                Produto produtoExistente = dao.buscarPorId(id);
+                if (produtoExistente == null) {
+                    response.status(404);
+                    return "{\"mensagem\":\"Produto não encontrado\"}";
+                }
+                
+                dao.deletar(id);
+                response.status(200);
+                return "{\"mensagem\":\"Produto Excluido com sucesso\"}";
+                
+            }
+        });
+
     }
+
+    
 
 }
